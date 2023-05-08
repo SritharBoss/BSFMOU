de4687f6db731466e171fd92d8dee1fa0b38b453
Srithar <sritharboss8@gmail.com>
2023-05-08 17:34:18 +0530

    Simplify code using brace expansion

--- mybin/organize_downloads.sh
+++ mybin/organize_downloads.sh
@@ -2,22 +2,23 @@
 
 dir='/mnt/Data/Downloads'
 
+
 #Moving Documents
-mv $dir/*.PDF $dir/*.txt $dir/*.pdf  /mnt/Data/Downloads/Documents 2>/dev/null
-mv $dir/*.xlsx $dir/*.doc $dir/*.docx $dir/*.xlx  /mnt/Data/Downloads/Documents/OfficeDocuments/ 2>/dev/null
+mv $dir/*.{pdf,PDF,txt}  $dir/Documents 2>/dev/null
+mv $dir/*.{xlsx,doc,docx,xlx}  $dir/Documents/OfficeDocuments/ 2>/dev/null
 
 #Images
-mv $dir/*.jpg $dir/*.jpeg $dir/*.svg $dir/*.png  /mnt/Data/Downloads/Pictures 2>/dev/null
+mv $dir/*.{jpg,jpeg,svg,png}  $dir/Pictures 2>/dev/null
 
 #Programs
-mv $dir/*.msi $dir/*.exe  /mnt/Data/Downloads/Programs/Windows 2>/dev/null
-mv $dir/*.deb  /mnt/Data/Downloads/Programs/Linux 2>/dev/null
+mv $dir/*.{msi,exe}  $dir/Programs/Windows 2>/dev/null
+mv $dir/*.{deb}  $dir/Programs/Linux 2>/dev/null
 
 #Compressed
-mv $dir/*.tar $dir/*.zip $dir/*.gz /mnt/Data/Downloads/Compressed 2>/dev/null
+mv $dir/*.{tar,zip,gz} $dir/Compressed 2>/dev/null
 
 #Torrent
-mv $dir/*.torrent /mnt/Data/Downloads/Torrent 2>/dev/null
+mv $dir/*.{torrent} $dir/Torrent 2>/dev/null
 
 #Misc
 
