444600b0937883fafca749648ae607a98bfd05fa
Srithar <39944141+SritharBoss@users.noreply.github.com>
2023-05-27 11:49:18 +0530

    Update organize.bat

--- organize.bat
+++ organize.bat
@@ -1,28 +1,53 @@
 @echo off
-set "sourceDirectory=D:\Downloads"
+set "sourceDirectory=C:\Users\cscpv\Downloads"
 
-for %%A in ("%sourceDirectory%\*.pdf") do (
-    move "%%A" "D:\Downloads\Pdf Documents"
+if not exist "%sourceDirectory%\Pdf Documents" (
+	mkdir "%sourceDirectory%\Pdf Documents"
 )
 
-for %%B in ("%sourceDirectory%\*.xlsx" "%sourceDirectory%\*.xlx" "%sourceDirectory%\*.doc" "%sourceDirectory%\*.docx") do (
-    move "%%B" "D:\Downloads\Office Documents"
+if not exist "%sourceDirectory%\Office Documents" (
+	mkdir "%sourceDirectory%\Office Documents"
+)
+
+if not exist "%sourceDirectory%\Compressed" (
+	mkdir "%sourceDirectory%\Compressed"
+)
+
+if not exist "%sourceDirectory%\Applications" (
+	mkdir "%sourceDirectory%\Applications"
+)
+
+if not exist "%sourceDirectory%\Images\WhatsApp Images" (
+	mkdir "%sourceDirectory%\Images\WhatsApp Images"
+)
+
+if not exist "%sourceDirectory%\Images" (
+	mkdir "%sourceDirectory%\Images"
+)
+
+
+for %%A in ("%sourceDirectory%\*.pdf") do (
+    move "%%A" "%sourceDirectory%\Pdf Documents"
+)
+
+for %%B in ("%sourceDirectory%\*.xlsx" "%sourceDirectory%\*.xlx" "%sourceDirectory%\*.doc" "%sourceDirectory%\*.docx" "%sourceDirectory%\*.csv") do (
+    move "%%B" "%sourceDirectory%\Office Documents"
 )
 
 for %%C in ("%sourceDirectory%\*.zip" "%sourceDirectory%\*.rar" "%sourceDirectory%\*.7z") do (
-    move "%%C" "D:\Downloads\Compressed"
+    move "%%C" "%sourceDirectory%\Compressed"
 )
 
 for %%D in ("%sourceDirectory%\*.exe" "%sourceDirectory%\*.msi") do (
-    move "%%D" "D:\Downloads\Applications"
+    move "%%D" "%sourceDirectory%\Applications"
 )
 
 for %%E in ("%sourceDirectory%\WhatsApp*.jpeg" "%sourceDirectory%\WhatsApp*.jpg" "%sourceDirectory%\WhatsApp*.png") do (
-    move "%%E" "D:\Downloads\Images\WhatsApp Images"
+    move "%%E" "%sourceDirectory%\Images\WhatsApp Images"
 )
 
 for %%E in ("%sourceDirectory%\*.jpeg" "%sourceDirectory%\*.jpg" "%sourceDirectory%\*.png") do (
-    move "%%E" "D:\Downloads\Images"
+    move "%%E" "%sourceDirectory%\Images"
 )
 
 echo All files moved successfully.
