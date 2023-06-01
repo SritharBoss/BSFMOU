c8b5c1833cbe1f5d26dfa72ce23ec4de93df03d3
Srithar <sritharboss8@gmail.com>
2023-06-01 11:45:43 +0530

    Win bin moved and updated organize downloads

--- mybin/organize_downloads.sh
+++ mybin/organize_downloads.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 dir='/mnt/Data/Downloads'
+logpath='/home/srithar/log/cron_status.txt'
 
 
 #Moving Documents
@@ -12,17 +13,18 @@ mv $dir/*.{jpg,jpeg,svg,png}  $dir/Pictures 2>/dev/null
 
 #Programs
 mv $dir/*.{msi,exe}  $dir/Programs/Windows 2>/dev/null
-mv $dir/*.{deb}  $dir/Programs/Linux 2>/dev/null
+mv $dir/*.deb  $dir/Programs/Linux 2>/dev/null
+mv $dir/*.apk  $dir/Programs/Android 2>/dev/null
 
 #Compressed
 mv $dir/*.{tar,zip,gz} $dir/Compressed 2>/dev/null
 
 #Torrent
-mv $dir/*.{torrent} $dir/Torrent 2>/dev/null
+mv $dir/*.torrent $dir/Torrent 2>/dev/null
 
 #Misc
 
 
-echo "$(date "+%m-%d-%Y %H:%M --- ")[organize_download] Successfully Completed" >> /home/srithar/log/cron_status.txt 2>&1
+echo "$(date "+%m-%d-%Y %H:%M --- ")[organize_download] Successfully Completed" >> $logpath 2>&1
 
 exit 0;
