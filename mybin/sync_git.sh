b9506f41fd36327ce581ba89d1f062d1924957e5
Srithar <sritharboss8@gmail.com>
2023-07-18 20:04:49 +0530

    Sync Git Updated]

--- mybin/sync_git.sh
+++ mybin/sync_git.sh
@@ -12,6 +12,8 @@ file="/mnt/Data/gitpath"
 
 [[ -f "$1" ]] && file="$1"
 
+echo -n "$(date "+%m-%d-%Y %H:%M --- ")" >> /home/srithar/log/cron_status.txt 2>&1
+
 
 cat "$file" | while read line 
 do
@@ -22,6 +24,8 @@ do
 			notify-send "Sync Pending $line" "$(git status -s)";
 		fi
 	else
-		echo "Error while pulling" && exit 1
+		echo "Error while pulling" >> /home/srithar/log/cron_status.txt; exit 1
 	fi
 done
+
+echo "Git Sync Done.." >> /home/srithar/log/cron_status.txt
